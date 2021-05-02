a3303788a8410f892f8472aeeff170d0bdd90583
evon27 <59104209+evon27@users.noreply.github.com>
2021-05-03 00:34:44 +0900

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ const IMG_NUM = 5;
 
 function paintImage(imgNumber) {
     const image = new Image();
-    image.src = `images/${imgNumber}.JPG`;
+    image.src = `images/${imgNumber}.jpg`;
     image.classList.add('bgImage');
     image.alt = `Can't load the image no ${imgNumber}`;
     body.prepend(image);
